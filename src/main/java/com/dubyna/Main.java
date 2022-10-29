c5f32892c361bc8c74fef65ddb17b1d878720436
Dubyna_Rostyslav <rostislavdubina94@gmail.com>
2022-10-29 10:22:34 +0300

    first commit

---
+++ src/main/java/com/dubyna/Main.java
@@ -0,0 +1,11 @@
+package com.dubyna;
+
+public class Main {
+    public static void main(String[] args) {
+        int y = 5;
+        for (int i = 0; i <= 10; i = i + 1) {
+            System.out.println(i + " Step " + y + " value ");
+            y = y +2;
+        }
+    }
+}
