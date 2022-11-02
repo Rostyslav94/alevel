74de05e65cc8ea282bea0071c658d508ba7867fa
Dubyna_Rostyslav <rostislavdubina94@gmail.com>
2022-11-02 14:52:13 +0200

    git commit

---
+++ src/main/java/com/dubyna/Main4.java
@@ -0,0 +1,12 @@
+package com.dubyna;
+
+public class Main4 {
+    public static void main  (String[] args) {
+        String str = "Hello World!";
+        char c = str.charAt(1-1);
+        char b = str.charAt(12-1);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
+
