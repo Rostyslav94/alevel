4dbb4e4468dc99820ac7170cd29bb3b1bc441e57
Dubyna_Rostyslav <rostislavdubina94@gmail.com>
2022-11-02 20:51:11 +0200

    h4

--- src/main/java/com/dubyna/Main4.java
+++ src/main/java/com/dubyna/Main4.java
@@ -1,5 +1,4 @@
 package com.dubyna;
-
 public class Main4 {
     public static void main  (String[] args) {
         String str = "Hello World!";
@@ -8,5 +7,14 @@ public class Main4 {
         System.out.println(c);
         System.out.println(b);
     }
+
+    private static void copy() {
+        String str = "Hello World!";
+        char n = str.charAt(1-1);
+        char k = str.charAt(12-1);
+        System.out.println(n);
+        System.out.println(k);
+    }
 }
+
 
